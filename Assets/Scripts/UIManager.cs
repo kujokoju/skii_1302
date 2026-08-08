12c072202a9a35ac901d5873ff63c657af22804b
Mais <salanya.tapech@gmail.com>
2026-08-08 14:53:32 +0700

    coin pack

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -7,6 +7,12 @@ public class UIManager : MonoBehaviour
     [SerializeField]
     private TMP_Text notiText;
 
+    [SerializeField]
+    private GameObject restartButton;
+
+    [SerializeField]
+    private Player player;
+
     public static UIManager instance;
 
      void Awake()
@@ -29,4 +35,17 @@ public class UIManager : MonoBehaviour
         notiText.text = s;
     }
 
+    public void RestartGame()
+    {
+        player.transform.position = new Vector3(0f, 87f, -85f);
+        player.HP = 100;
+        ShowNotiText("Restart");
+        Time.timeScale = 1f;
+        ShowHideSestartButton(false);
+    }
+
+    public void ShowHideSestartButton(bool flag)
+    {
+        restartButton.SetActive(flag);
+    }
 }
